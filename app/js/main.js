91b6172ab1660434fac6e8fdc080b65331a6beb0
LexaLP <fan.lp95@gmail.com>
2021-11-20 14:11:27 +0300

    добавлены ховер эфффекты

--- app/js/main.js
+++ app/js/main.js
@@ -3,10 +3,10 @@ $(function () {
  $('.top-slider__inner').slick({
   arrows: true,
   prevArrow: '<button type="button" class="slick-prev"><img src="../images/slider_bg/arrow-left.svg" alt="left"></button>',
-  nextArrow: '<button type="button" class="slick-next"><img src="../images/slider_bg/arrow-right.svg" alt="right"></button>'
+  nextArrow: '<button type="button" class="slick-next"><img src="../images/slider_bg/arrow-right.svg" alt="right"></button>',
   fade: true,
-  autoplay: true,
-  autoplaySpeed: 3000
+  // autoplay: true,
+  // autoplaySpeed: 3000
  });
 
 });

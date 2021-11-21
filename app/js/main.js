6c53461581315eadef2b055cc610aadbe6cb2975
LexaLP <fan.lp95@gmail.com>
2021-11-21 20:54:09 +0300

    добавлены топовые товары

--- app/js/main.js
+++ app/js/main.js
@@ -4,7 +4,7 @@ $(function () {
   arrows: true,
   prevArrow: '<button type="button" class="slick-prev"><img src="../images/slider_bg/arrow-left.svg" alt="left"></button>',
   nextArrow: '<button type="button" class="slick-next"><img src="../images/slider_bg/arrow-right.svg" alt="right"></button>',
-  fade: true,
+  fade: true
   // autoplay: true,
   // autoplaySpeed: 3000
  });

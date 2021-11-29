5f5e5c04c2e0afbd2a2a993cb070f90f4d535a48
LexaLP <fan.lp95@gmail.com>
2021-11-29 19:32:07 +0300

    доделано дз+исправлены ошибки

--- app/js/main.js
+++ app/js/main.js
@@ -8,14 +8,54 @@ $(function () {
   infinite: false
  });
 
+
+
+ const cropElement = document.querySelectorAll('.product__name'), // выбор элементов  
+  size = 92 // кол-во символов  
+ endCharacter = '...'; // окончание  
+
+ cropElement.forEach(el => {
+  let text = el.innerHTML;
+
+  if (el.innerHTML.length > size) {
+   text = text.substr(0, size);
+   el.innerHTML = text + endCharacter;
+  }
+ });
+
  $(".star").rateYo({
   numStars: 1,
   starWidth: "16px",
-  readOnly:true
-  });
+  readOnly: true
+ });
 
- var mixer = mixitup('.top-items__content');
- var mixer = mixitup('.sale__content');
+ var top = document.querySelector('[data-ref="mix1"]');
+ var sale = document.querySelector('[data-ref="mix2"]');
+
+ var config = {
+  controls: {
+   scope: 'local'
+  }
+ };
 
 
+ var mixer1 = mixitup(top, config);
+ var mixer1 = mixitup(sale, config);
 });
+
+
+$(function () {
+ const cropElement = document.querySelectorAll('.sale__wrapper .product__name'), // выбор элементов  
+  size = 55 // кол-во символов  
+ endCharacter = '...'; // окончание  
+
+ cropElement.forEach(el => {
+  let text = el.innerHTML;
+
+  if (el.innerHTML.length > size) {
+   text = text.substr(0, size);
+   el.innerHTML = text + endCharacter;
+  }
+ });
+
+})
